0d029e768a6232f7a9672d848515fdcb2a210aa2
USUARIO <USUARIO@LABORATORIO-PC>
2019-09-17 08:36:42 -0300

    inicialização do projeto

---
+++ src/Qacademico/Professor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Qacademico;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Professor {
+    private  String cursos;
+    private String salario;
+
+    public String getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(String cursos) {
+        this.cursos = cursos;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+    
+}
